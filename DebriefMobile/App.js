8ad9e08d188b4d314379cf1cf71904bb072f2a81
joshua-le <jdl8176@nyu.edu>
2023-04-13 00:52:07 -0400

    made it work

--- DebriefMobile/App.js
+++ DebriefMobile/App.js
@@ -1,25 +1,48 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { SafeAreaView, View, TouchableOpacity, StyleSheet, ScrollView, Dimensions, AppState } from 'react-native';
-import Header from './components/Header';
-import SummaryBriefing from './components/SummaryBriefing';
-import LottieView from 'lottie-react-native';
-import data from './dummyData';
+import React, { useState, useRef, useEffect } from "react";
+import {
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  AppState,
+  Modal,
+  TextInput,
+} from "react-native";
+import Header from "./components/Header";
+import SummaryBriefing from "./components/SummaryBriefing";
+import LottieView from "lottie-react-native";
+import data from "./dummyData";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const TEXT_FONT_SIZE = 16;
 
 const App = () => {
   const [showLottie, setShowLottie] = useState(true);
   const [appState, setAppState] = useState(AppState.currentState);
+  const [modalVisible, setModalVisible] = useState(true);
   const scrollViewRef = useRef(null);
+  const secondLottieRef = useRef(null);
   const lottieRef = useRef(null);
+  const [userText, setUserText] = useState("");
+  const [inputFocused, setInputFocused] = useState(false);
+  const [showSecondLottie, setShowSecondLottie] = useState(false);
+  const [showFirstLottie, setShowFirstLottie] = useState(true);
+  const [showText, setShowText] = useState(false);
+  const [showHeader, setShowHeader] = useState(false);
 
   const handlePress = () => {
-    setShowLottie(false);
+    setShowFirstLottie(false);
+    setShowText(true);
   };
+  
 
   const onWordRevealed = (index) => {
     const wordHeight = 24;
-    const wordsPerRow = Math.floor(Dimensions.get('window').width / (TEXT_FONT_SIZE + 8));
+    const wordsPerRow = Math.floor(
+      Dimensions.get("window").width / (TEXT_FONT_SIZE + 8)
+    );
     const row = Math.floor(index / wordsPerRow);
 
     if (scrollViewRef.current) {
@@ -28,7 +51,7 @@ const App = () => {
   };
 
   const handleAppStateChange = (nextAppState) => {
-    if (nextAppState === 'active' && appState !== 'active') {
+    if (nextAppState === "active" && appState !== "active") {
       setAppState(nextAppState);
       setShowLottie(true);
       lottieRef.current?.reset();
@@ -38,8 +61,15 @@ const App = () => {
     }
   };
 
+  const handleNext = () => {
+    setModalVisible(false);
+  };
+
   useEffect(() => {
-    const appStateSubscription = AppState.addEventListener('change', handleAppStateChange);
+    const appStateSubscription = AppState.addEventListener(
+      "change",
+      handleAppStateChange
+    );
 
     return () => {
       appStateSubscription.remove();
@@ -49,20 +79,80 @@ const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <Header name={data.name} />
-        {showLottie && (
-          <TouchableOpacity onPress={handlePress} style={styles.lottieContainer}>
+        {showHeader && <Header name={data.name} />}
+        {/* <Header name={data.name} /> */}
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.modalContainer}>
+            <TextInput
+              style={[
+                styles.input,
+                inputFocused && { borderBottomColor: "#8DBAE2" }, // Change borderBottomColor when focused
+              ]}
+              placeholder="Type your persona here"
+              onChangeText={(text) => setUserText(text)}
+              onFocus={() => setInputFocused(true)} // Set inputFocused to true when TextInput is focused
+              onBlur={() => setInputFocused(false)} // Set inputFocused to false when TextInput is blurred
+            />
+            <TouchableOpacity
+              onPress={() => { //THIS IS WHERE API IS CALLED
+                // USERTEXT IS WHERE PERSONA IS STORED
+                console.log(userText); // Log the user text when the next button is pressed
+                setModalVisible(!modalVisible);
+                setShowSecondLottie(true);
+                setShowFirstLottie(false);
+                setTimeout(() => {
+                  setShowSecondLottie(false);
+                  setShowFirstLottie(true);
+                  setShowHeader(true);
+                }, 10000); // THIS IS WHERE YOU CHANGE THE LOADING TIME
+              }}
+            >
+              <Icon
+                style={styles.nextButton}
+                name="arrow-forward"
+                size={45}
+                color="#8DBAE2"
+              />
+            </TouchableOpacity>
+          </View>
+        </Modal>
+        {showSecondLottie && (
+          <TouchableOpacity style={styles.lottieContainer}>
             <LottieView
-              ref={lottieRef}
-              onLayout={() => lottieRef.current?.play()}
-              source={require('./assets/startDebrief.json')}
+              ref={secondLottieRef}
+              onLayout={() => secondLottieRef.current?.play()}
+              source={require("./assets/loading.json")}
               loop
             />
           </TouchableOpacity>
         )}
-        {!showLottie && (
+
+        {showFirstLottie && (
+          <TouchableOpacity
+            onPress={handlePress}
+            style={styles.lottieContainer}
+          >
+            <LottieView
+              ref={lottieRef}
+              onLayout={() => lottieRef.current?.play()}
+              source={require("./assets/startDebrief.json")}
+              loop
+            />
+          </TouchableOpacity>
+        )}
+        {showText && (
           <ScrollView ref={scrollViewRef}>
-            <SummaryBriefing text={data.summaryBriefing} onWordRevealed={onWordRevealed} />
+            <SummaryBriefing
+              text={data.summaryBriefing}
+              onWordRevealed={onWordRevealed}
+            />
           </ScrollView>
         )}
       </View>
@@ -73,19 +163,37 @@ const App = () => {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   container: {
     flex: 1,
   },
   lottieContainer: {
-    position: 'absolute',
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+    width: "80%",
+    marginBottom: 20,
+    fontSize: 18,
+    paddingBottom: 14,
+  },
+  nextButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20, // Increase the margin top to move the arrow down
   },
 });
 
